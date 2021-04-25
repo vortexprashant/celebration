2dc63d33e110c35732f59a89bf2115dd73edcfbe
vortexprashant <vortexprashant@gmail.com>
2021-04-25 18:15:48 +0545

    commit 12

--- backend/models/product.js
+++ backend/models/product.js
@@ -10,7 +10,7 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: [true, 'Please enter product price'],
-        maxLength: [5, 'Product name cannot exceed 5 characters'],
+        maxLength: [10, 'Product name cannot exceed 5 characters'],
         default: 0.0
     },
     description: {
